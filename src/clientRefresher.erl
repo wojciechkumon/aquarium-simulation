c4606c6f8a208abea0d18b3650149fe79f493e3d
Wojciech Kumoń <wojtas626@gmail.com>
2017-01-14 16:58:40 +0100

    Working client and server

---
+++ src/clientRefresher.erl
@@ -0,0 +1,34 @@
+-module(clientRefresher).
+
+-export([startAquariumRefresher/1]).
+
+%% Aquarium state refresher in client
+
+startAquariumRefresher(SocketHandler) ->
+  Timer = time:startTime(self(), 150),
+  refresherLoop(SocketHandler, Timer).
+
+refresherLoop(SocketHandler, Timer) ->
+  receive
+    timeStep ->
+      handleRefresh(SocketHandler, Timer),
+      refresherLoop(SocketHandler, Timer);
+    {stop, Pid} ->
+      timer:cancel(Timer),
+      Pid ! stopped
+  end.
+
+handleRefresh(SocketHandler, Timer) ->
+  SocketHandler ! {checkAquariumState, self()},
+  receive
+    connectionClosed ->
+      closeRefresher(Timer);
+    error ->
+      closeRefresher(Timer);
+    Response ->
+      io:format("Aquarium refresher response: ~p~n", [Response])
+  end.
+
+closeRefresher(Timer) ->
+  io:format("[ClientRefresher] Connection closed~n"),
+  timer:cancel(Timer).
